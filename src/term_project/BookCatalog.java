2ebf19c45db60e6ea49d9c5ed4480ec3a8611784
Wenting Ye <Wenting_Ye@Outlook.com>
2017-01-03 16:36:55 +0800

    add BookCatalog

---
+++ src/term_project/BookCatalog.java
@@ -0,0 +1,19 @@
+package term_project;
+
+import java.util.HashMap;
+
+/**
+ * Created by YeWenting on 2017/1/3.
+ */
+public class BookCatalog
+{
+    /* The data field */
+    HashMap<String, ProductSpecification> books = new HashMap<String, ProductSpecification>();
+
+    /* The constructor method */
+    public BookCatalog() {}
+
+    /* The data access wrapper */
+    public ProductSpecification add(ProductSpecification book) { books.put(book.getISBN(), book); return book; }
+    public ProductSpecification get(String ISBN) { return books.get(ISBN); }
+}
